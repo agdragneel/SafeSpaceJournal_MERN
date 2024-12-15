8dc56836236cfb2c53b9a6d3cf13bdcc475c686d
noname <nomail@nomail.com>
2024-12-15 22:30:15 +0530

    Navbar Change 2

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,21 +1,24 @@
 // src/components/Navbar.js
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Navbar.css'
+import './Navbar.css';
 
 const Navbar = ({ user, onLogout }) => {
   return (
     <nav className="navbar">
+      <div className="navbar-logo">
+        <Link className="navbar-logo-link" to="/">SafeSpace</Link>
+      </div>
       <ul className="navbar-list">
         <li className="navbar-item"><Link className="navbar-link" to="/">Home</Link></li>
         <li className="navbar-item"><Link className="navbar-link" to="/about">About</Link></li>
         <li className="navbar-item"><Link className="navbar-link" to="/contact">Contact</Link></li>
         {user ? (
           <li className="navbar-item">
-            <Link className="navbar-link " to="/" onClick={onLogout}>Logout</Link>
+            <Link className="navbar-link navbar-button" to="/" onClick={onLogout}>Logout</Link>
           </li>
         ) : (
-          <li className="navbar-item"><Link className="navbar-link" to="/login">Login/Register</Link></li>
+          <li className="navbar-item"><Link className="navbar-link" to="/login">Sign In</Link></li>
         )}
       </ul>
     </nav>
