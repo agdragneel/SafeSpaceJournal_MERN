d4d7b7728d44a1235e58e1e05b395b49232be2f1
noname <nomail@nomail.com>
2024-12-16 11:45:58 +0530

    Completed Design of HomePage.js with CSS, Framer Motion, Particles.

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -3,6 +3,7 @@ import React from 'react';
 import ParticlesComponent from '../components/Particles'; // Import the Particles component
 import './HomePage.css'; // Import custom CSS for the page
 import { motion } from 'framer-motion';
+import { Link } from 'react-router-dom'; // Import Link for routing
 
 const HomePage = () => {
   return (
@@ -21,7 +22,9 @@ const HomePage = () => {
           className="first-motion"
           initial={{ opacity: 0 }}
           whileInView={{ opacity: 1 }}
-          transition={{duration: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 1 }}
+          viewport={{ once: false, amount: 0.1 }}
         >
           <p>
             Welcome to your sanctuary, a space where your thoughts can flow freely, untouched by judgment or whispers.
@@ -32,8 +35,11 @@ const HomePage = () => {
           className="spaced-motion"
           initial={{ opacity: 0 }}
           whileInView={{ opacity: 1 }}
-          transition={{duration: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 1 }}
+          viewport={{ once: false, amount: 0.1 }}
         >
+          <img src="https://i.ibb.co/Lrc90xZ/Constellation-Circle2.png" alt="Constellation_circle" />
           <p>
             This is more than just a journal; it's a compassionate listener powered by AI. As you pour out your thoughts, you can ask for gentle analysis to uncover patterns, insights, and pathways to healing. It’s therapy-like feedback, thoughtful and understanding, always there to help you navigate your mind.
           </p>
@@ -43,10 +49,12 @@ const HomePage = () => {
         </motion.div>
 
         <motion.div
-          className="spaced-motion"
+          className="spaced-motion2"
           initial={{ opacity: 0 }}
           whileInView={{ opacity: 1 }}
-          transition={{duration: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 1 }}
+          viewport={{ once: false, amount: 0.1 }}
         >
           <p>
             Feeling overwhelmed or just want to talk? Our AI-powered chatbot is ready to listen. It’s a conversation without pressure, a companion that responds with empathy and understanding whenever you need it most.
@@ -54,6 +62,11 @@ const HomePage = () => {
           <p>
             This is your space to write, to heal, and to connect with the support you deserve. Your thoughts are safe here.
           </p>
+
+          {/* Get Started Button */}
+          <Link className="get-started-button" to="/login">
+            Get Started
+          </Link>
         </motion.div>
       </div>
     </div>
